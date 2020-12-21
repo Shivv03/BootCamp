2510aec068a21bf8fd0183b39f9160496680e953
Neelakandan <neelakandans@cns-inc.com>
2020-12-21 13:12:28 +0530

    changed js implementation

--- week-8/callback-hell/script.js
+++ week-8/callback-hell/script.js
@@ -3,91 +3,50 @@ let body = document.body;
 document.body.append(paragraph);
 paragraph.setAttribute('class','countdown');
 
-const startCountDown = (cb) => {
     paragraph.innerHTML = '10';
     body.style.backgroundColor = getRandomColor();
-    cb(countdownTimer8);
-}
-
-const countdownTimer9 = (cb) => {
     setTimeout(() => {
         paragraph.innerHTML = '9';
         body.style.backgroundColor = getRandomColor();
-        cb(countdownTimer7);
+        setTimeout(() => {
+            paragraph.innerHTML = '8';
+            body.style.backgroundColor = getRandomColor();
+            setTimeout(() => {
+                paragraph.innerHTML = '7';
+                body.style.backgroundColor = getRandomColor();
+                setTimeout(() => {
+                    paragraph.innerHTML = '6';
+                    body.style.backgroundColor = getRandomColor();
+                    setTimeout(() => {
+                        paragraph.innerHTML = '5';
+                        body.style.backgroundColor = getRandomColor();
+                        setTimeout(() => {
+                            paragraph.innerHTML = '4';
+                            body.style.backgroundColor = getRandomColor();
+                            setTimeout(() => {
+                                paragraph.innerHTML = '3';
+                                body.style.backgroundColor = getRandomColor();
+                                setTimeout(() => {
+                                    paragraph.innerHTML = '2';
+                                    body.style.backgroundColor = getRandomColor();
+                                    setTimeout(() => {
+                                        paragraph.innerHTML = '1';
+                                        body.style.backgroundColor = getRandomColor();
+                                        setTimeout(()=> {
+                                            body.style.backgroundColor = getRandomColor();
+                                            paragraph.innerHTML = 'Happy Independence Day!';
+                                            paragraph.style.fontWeight = 'bold';
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
     },1000)
-}
 
-const countdownTimer8 = (cb) => {
-    setTimeout(() => {
-        paragraph.innerHTML = '8';
-        body.style.backgroundColor = getRandomColor();
-        cb(countdownTimer6)
-    },1000)
-}
-
-const countdownTimer7 = (cb) => {
-    setTimeout(() => {
-        paragraph.innerHTML = '7';
-        body.style.backgroundColor = getRandomColor();
-        cb(countdownTimer5)
-    },1000)
-}
-
-const countdownTimer6 = (cb) => {
-    setTimeout(() => {
-        paragraph.innerHTML = '6';
-        body.style.backgroundColor = getRandomColor();
-        cb(countdownTimer4)
-    },1000)
-}
-
-const countdownTimer5 = (cb) => {
-    setTimeout(() => {
-        paragraph.innerHTML = '5';
-        body.style.backgroundColor = getRandomColor();
-        cb(countdownTimer3)
-    },1000)
-}
-
-const countdownTimer4 = (cb) => {
-    setTimeout(() => {
-        paragraph.innerHTML = '4';
-        body.style.backgroundColor = getRandomColor();
-        cb(countdownTimer2)
-    },1000)
-}
-
-const countdownTimer3 = (cb) => {
-    setTimeout(() => {
-        paragraph.innerHTML = '3';
-        body.style.backgroundColor = getRandomColor();
-        cb(countdownTimer1)
-    },1000)
-}
-
-const countdownTimer2 = (cb) => {
-    setTimeout(() => {
-        paragraph.innerHTML = '2';
-        body.style.backgroundColor = getRandomColor();
-        cb(displayWishes)
-    },1000)
-}
-
-const countdownTimer1 = (cb) => {
-    setTimeout(() => {
-        paragraph.innerHTML = '1';
-        body.style.backgroundColor = getRandomColor();
-        cb()
-    },1000)
-}
-
-const displayWishes = () => {
-    setTimeout(()=> {
-        body.style.backgroundColor = getRandomColor();
-        paragraph.innerHTML = 'Happy Independence Day!';
-        paragraph.style.fontWeight = 'bold';
-    },1000)
-}
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
