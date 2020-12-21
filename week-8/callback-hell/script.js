f8c0b1319fd74d0e0597db565568c20f35b68709
Neelakandan <neelakandans@cns-inc.com>
2020-12-21 16:38:09 +0530

    css changes

--- week-8/callback-hell/script.js
+++ week-8/callback-hell/script.js
@@ -1,41 +1,53 @@
-let paragraph = document.createElement('p');
+let timerText = document.createElement('h1');
 let body = document.body;
-document.body.append(paragraph);
-paragraph.setAttribute('class','countdown');
+document.body.append(timerText);
+timerText.setAttribute('class','countdown');
 
-    paragraph.innerHTML = '10';
+const getRandomColor = () => {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+timerText.innerHTML = '10';
+body.style.backgroundColor = getRandomColor();
+setTimeout(() => {
+    timerText.innerHTML = '9';
     body.style.backgroundColor = getRandomColor();
     setTimeout(() => {
-        paragraph.innerHTML = '9';
+        timerText.innerHTML = '8';
         body.style.backgroundColor = getRandomColor();
         setTimeout(() => {
-            paragraph.innerHTML = '8';
+            timerText.innerHTML = '7';
             body.style.backgroundColor = getRandomColor();
             setTimeout(() => {
-                paragraph.innerHTML = '7';
+                timerText.innerHTML = '6';
                 body.style.backgroundColor = getRandomColor();
                 setTimeout(() => {
-                    paragraph.innerHTML = '6';
+                    timerText.innerHTML = '5';
                     body.style.backgroundColor = getRandomColor();
                     setTimeout(() => {
-                        paragraph.innerHTML = '5';
+                        timerText.innerHTML = '4';
                         body.style.backgroundColor = getRandomColor();
                         setTimeout(() => {
-                            paragraph.innerHTML = '4';
+                            timerText.innerHTML = '3';
                             body.style.backgroundColor = getRandomColor();
                             setTimeout(() => {
-                                paragraph.innerHTML = '3';
+                                timerText.innerHTML = '2';
                                 body.style.backgroundColor = getRandomColor();
                                 setTimeout(() => {
-                                    paragraph.innerHTML = '2';
+                                    timerText.innerHTML = '1';
                                     body.style.backgroundColor = getRandomColor();
-                                    setTimeout(() => {
-                                        paragraph.innerHTML = '1';
+                                    setTimeout(()=> {
                                         body.style.backgroundColor = getRandomColor();
+                                        timerText.innerHTML = 'Yaay!';
                                         setTimeout(()=> {
                                             body.style.backgroundColor = getRandomColor();
-                                            paragraph.innerHTML = 'Happy Independence Day!';
-                                            paragraph.style.fontWeight = 'bold';
+                                            timerText.innerHTML = 'Happy Independence Day!';
+                                            timerText.style.fontSize = '50px';
                                         },1000)
                                     },1000)
                                 },1000)
@@ -46,16 +58,8 @@ paragraph.setAttribute('class','countdown');
             },1000)
         },1000)
     },1000)
+},1000)
 
 
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
 
-startCountDown(countdownTimer9);
 
