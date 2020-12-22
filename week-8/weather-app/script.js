747f4e1b77a955a02d14fa2d847c56a95e7bc969
Neelakandan <neelakandans@cns-inc.com>
2020-12-23 02:17:37 +0530

    weather state changes added

--- week-8/weather-app/script.js
+++ week-8/weather-app/script.js
@@ -1,5 +1,6 @@
 var countryCardObj;
 var columns = [];
+const API_key = 'c408268efc3dbdf97760c4d05926d1eb';
 
 const createElement = (element , classes , parentElement) =>{
     let createdElement = document.createElement(element);
@@ -16,26 +17,40 @@ let row = createElement('div', 'row', container);
 
 
 const createWeatherCard = (country) =>  {
-    let cardContainer = createElement('div', 'col-lg-4 col-12 px-5 py-2', row);
+    let cardContainer = createElement('div', 'col-lg-4 col-12 px-4 py-2', row);
     let card = createElement('div', 'card border-2 border-dark rounded-3 bg-custom', cardContainer);
-    let cardHeader = createElement('div', 'card-header bg-dark text-white', card);
-    let cardHeaderContent = createElement('h5', 'card-title text-center', cardHeader);
-    cardHeaderContent.innerHTML = country.name;
-    let cardBody = createElement('div', 'card-body d-flex flex-column align-items-center justify-content-center', card);
-    let flagImg = createElement('img', 'image', cardBody);
-    flagImg.src = country.flag;
-    flagImg.alt = "display-picture";
-    flagImg.setAttribute('class','flag m-1')
-    let countryCapital = createElement('p', 'card-text text-white m-0', cardBody);
-    countryCapital.innerHTML = `Capital: ${country.capital}`
-    let countryRegion = createElement('p', 'card-text text-white m-0', cardBody);
-    countryRegion.innerHTML = `Region: ${country.region}`
-    let countryCode = createElement('p', 'card-text text-white m-0', cardBody);
-    countryCode.innerHTML = `Country Code: ${country.alpha3Code}`
-    let btn = createElement('input', 'image', cardBody);
-    btn.type = 'button'
-    btn.setAttribute('class','btn btn-primary m-1')
-    btn.value = 'Click for Weather';
+        let cardHeader = createElement('div', 'card-header bg-dark text-white', card);
+            let cardHeaderContent = createElement('h5', 'card-title text-center align-items-center', cardHeader);
+            cardHeaderContent.innerHTML = `${country.name} (${country.alpha3Code})`;
+        let cardBody = createElement('div', 'card-body d-flex flex-column align-items-center justify-content-center', card);
+            let flagImg = createElement('img', 'image', cardBody);
+                flagImg.src = country.flag;
+                flagImg.alt = "display-picture";
+                flagImg.setAttribute('class','flag m-1')
+            let countryInfo = createElement('div', 'd-flex flex-column align-items-center justify-content-center', cardBody);
+                let countryCapital = createElement('p', 'card-text text-white m-0', countryInfo);
+                    countryCapital.innerHTML = `Capital: ${country.capital}`
+                let countryRegion = createElement('p', 'card-text text-white m-0', countryInfo);
+                    countryRegion.innerHTML = `Region: ${country.region}`
+                let countryCode = createElement('p', 'card-text text-white m-0', countryInfo);
+                    countryCode.innerHTML = `Population: ${country.population}`
+                let btn = createElement('input', '', countryInfo);
+                    btn.type = 'button'
+                    btn.setAttribute("onclick",`displayWeather(${country.latlng},event)`)
+                    btn.setAttribute('class','btn btn-primary m-1')
+                    btn.value = 'Click for Weather';
+            let weatherInfo = createElement('div', 'd-flex flex-column align-items-center justify-content-center d-none', cardBody);
+            weatherInfo.innerHTML = `<div class="loader d-flex justify-content-center m-2">
+                                                <div class="spinner-border text-light" style="width: 4rem; height: 4rem;" role="loader">
+                                                        <span class="visually-hidden">Loading...</span>
+                                                </div>
+                                            </div>
+                                            <div class="weatherInfoContent "></div>`
+            let btn2 = createElement('button', 'd-none', weatherInfo);
+            btn2.setAttribute('onclick', `resetState(event)`)
+            btn2.setAttribute('class','btn btn-secondary m-1')
+            btn2.innerHTML = 'Reset';
+
     countryCardObj = { country : country.name, code: country.alpha3Code , card: cardContainer}
     columns.push(countryCardObj);
 }
@@ -46,7 +61,7 @@ fetch('https://restcountries.eu/rest/v2/all')
 })
 .then((data)=> {
     data.forEach(country => {
-        if(country.name == 'Afghanistan123'){
+        if(country.name == 'Afghanistan'){
             console.log(country)
         }  
         createWeatherCard(country)
@@ -54,7 +69,6 @@ fetch('https://restcountries.eu/rest/v2/all')
 })
 
 const search = document.getElementById('search');
-console.log(search);
 search.addEventListener("keyup", function(){
     searchbyCountry(this.value);
 })
@@ -85,6 +99,40 @@ const searchbyCountry = (searchText) => {
         infoText.classList.add("d-none");
     }
 
+}
+
+const displayWeather = (lat,long,event) => {
+    let card = event.path.find(elem => elem.classList && elem.classList.contains("card"))
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_key}&units=metric`)
+    .then(resp=> resp.json())
+    .then(data=> {
+        let countryInfo = card.children[1].children[1];
+        countryInfo.classList.add('d-none')
+        let weatherInfo = card.children[1].children[2];
+            weatherInfo.children[0].classList.add('d-none')
+            let weatherInfoContent =  weatherInfo.children[1];
+            let temp = createElement('h5', 'card-text text-white text-center mt-1', weatherInfoContent);
+            temp.innerHTML = `<i class="far fa-cloud"></i> ${data.main.temp}<sup>°C</sup>`;
+            let feelsLike = createElement('p', 'card-text text-white m-0 text-center', weatherInfoContent);
+            feelsLike.innerHTML = `Feels Like :  ${data.main.feels_like}<sup>°C</sup>`;
+            let subSection = createElement('div', 'd-flex align-content-between', weatherInfoContent);
+                let tempMin = createElement('p', 'card-text text-white mx-2 my-0', subSection);
+                    tempMin.innerHTML = `Min : ${data.main.temp_min}<sup>°C</sup>`;
+                let tempMax = createElement('p', 'card-text text-white mx-2 my-0', subSection);
+                    tempMax.innerHTML = `Max : ${data.main.temp_max}<sup>°C</sup>`;
+            
+        weatherInfo.classList.remove('d-none')
+    })
+};
+
+const resetState = (event) => {
+    let card = event.path.find(elem => elem.classList && elem.classList.contains("card"))
+    let countryInfo = card.children[1].children[1];
+    countryInfo.classList.remove('d-none')
+    let weatherInfo = card.children[1].children[2];
+    weatherInfo.children[0].classList.remove('d-none')
+    weatherInfo.classList.add('d-none')
+    weatherInfo.children[1].innerHTML = '';
 }
 
 
